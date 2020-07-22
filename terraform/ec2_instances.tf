64dd3824e295a0e3ee2ea8bdb6531f70c6364a7c
Saverio Miroddi <saverio.pub2@gmail.com>
2020-07-22 19:33:30 +0200

    Duplicate resource in preparation for load balancing

--- terraform/ec2_instances.tf
+++ terraform/ec2_instances.tf
@@ -14,3 +14,20 @@ resource "aws_instance" "first_instance" {
 
   user_data = file("${path.module}/ec2_instances_user_data.sh")
 }
+
+resource "aws_instance" "second_instance" {
+  ami           = "ami-079024c517d22af5b"
+  instance_type = "t2.micro"
+
+  subnet_id = aws_subnet.main2.id
+  vpc_security_group_ids = [
+    aws_security_group.main.id,
+  ]
+  associate_public_ip_address = true
+
+  key_name = "saverio"
+
+  iam_instance_profile = aws_iam_role.demo_role.id
+
+  user_data = file("${path.module}/ec2_instances_user_data.sh")
+}
